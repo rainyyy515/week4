bf269cc344023edc8a963c15820cfb79337349bf
yanwei <yanwei3623918@gmail.com>
2022-08-08 23:25:24 +0800

    Update 2022-08-08T15:25:18.950Z

--- app/assets/js/all.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-  $('.showNavbar').on('click', function (e) {
-    e.preventDefault();
-    $('.navbar').toggleClass('navbar-show');
-    // console.log('點擊到');
-  });
-});
---
+++ assets/js/all.js
@@ -0,0 +1,9 @@
+"use strict";
+
+$(document).ready(function () {
+  $('.showNavbar').on('click', function (e) {
+    e.preventDefault();
+    $('.navbar').toggleClass('navbar-show'); // console.log('點擊到');
+  });
+});
+//# sourceMappingURL=all.js.map
